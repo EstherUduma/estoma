ceb441d8e4bb1dba18aa65fd50e58f520664100c
EstherUduma <estheruduma23@gmail.com>
2024-08-19 17:26:08 +0100

    working on destination array and map

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,9 +1,103 @@
 import React from "react";
 import './main.css'
 
+//Array map named data
+const Data = [
+    {
+        id:1,
+        imgSrc: img,
+        destTitle: 'Bora Bora',
+        location: 'New Zealand',
+        grade: 'CULTURAL RELAX',
+        fees: '$700',
+        description: 'The epitome of romance, Bora Bora  is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities.'
+    },
+
+    {
+        id:2,
+        imgSrc: img2,
+        destTitle: 'Machu Picchu',
+        location: 'Peru',
+        grade: 'CULTURAL RELAX',
+        fees: '$600',
+        description: 'Huayna  Picchu is a mountain in Peru, rising over Machu Picchu, the so-called Lost City of Incas. This place is popular among tourists as the sunrise from the Sun Gate is simply spectacular.' 
+    },
+
+    {
+        id:3,
+        imgSrc: img3,
+        destTitle: 'Great Barrier Reef',
+        location: 'Australia',
+        grade: 'CULTURAL RELAX',
+        fees: '$700',
+        description: 'One of the most remarkable Australian natural gifts is the  Great Barrier Reef. The hallmark of this place is \'lavish\' and \'beauty\'. Always interesting to be in this place.'
+    },
+
+    {
+        id:4,
+        imgSrc: img4,
+        destTitle: 'Cappadocia',
+        location: 'Turkey',
+        grade: 'CULTURAL RELAX',
+        fees: '$800',
+        description: 'According to the World Tourism Ranking, 45 Million people visit Turkey each year, and from that about 2 Million come to visit Cappadocia. This place is known for its luxurious stays and adventurous activities.'
+    },
+
+    {
+        id:5,
+        imgSrc: img5,
+        destTitle: 'Guanajuato',
+        location: 'Mexico',
+        grade: 'CULTURAL RELAX',
+        fees: '$1100',
+        description: 'A city in central Mexico, Guanajuato is known for its history of silver mining and colonial architecture. The houses in the city are very attractively painted with the most bright colors available. Always welcome.'
+    },
+
+    {
+        id:6,
+        imgSrc: img6,
+        destTitle: 'Cinque Terre',
+        location: 'Italy',
+        grade: 'CULTURAL RELAX',
+        fees: '$840',
+        description: 'The vibrant hues of of the houses are its benchmark and explain the beauty of this place. Also, if you are a foodie and love seafood, you will be exhilarated with  the choice you are about to get here. Happy  exploring great food!'
+    },
+
+    {
+        id:7,
+        imgSrc: img7,
+        destTitle: 'Angkor Wat',
+        location: 'Cambodia',
+        grade: 'CULTURAL RELAX',
+        fees: '$790',
+        description: 'Angkot wat represents  the entire range of Khmer art from the 9th to the 15th century. This temple is considered the  heart and soul of Cambodia. This place is known for its luxurious stays and adventurous activities.' 
+    },
+
+    {
+        id:8,
+        imgSrc: img8,
+        destTitle: 'Taj Mahal',
+        location: 'India',
+        grade: 'CULTURAL RELAX',
+        fees: '$900',
+        description: 'An immense mausoleum of white marble '
+    }
+]
+
 const Main = () => {
     return (
-        <div>main</div>
+        <section className="main container section">
+            <div className="secTitle">
+                <h3 className="title">
+                    Most visited destinations
+                </h3>
+            </div>
+
+            <div className="secContent">
+                {/* Using high array method (map) */}
+
+            </div>
+        </section>
     )
 }
 
