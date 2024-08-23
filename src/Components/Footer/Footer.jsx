bea2dae180fcd0d0906c852805d9528bff431f5f
EstherUduma <estheruduma23@gmail.com>
2024-08-23 12:15:13 +0100

    footer completed for media  query

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,158 @@
 import React from "react";
 import './footer.css'
+import video2 from '../../Assets/video0.mp4'
+import { FiChevronRight, FiSend } from "react-icons/fi";
+import { MdOutlineTravelExplore } from 'react-icons/md';
+import { AiFillInstagram, AiFillYoutube, AiOutlineTwitter } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div>footer</div>
+        <section className="footer">
+            <div className="videoDiv">
+                <video src={video2} loop autoPlay muted type="video/mp4"></video>
+            </div>
+
+            <div className="secContent container">
+                <div className="contactDiv flex">
+                    <div className="text">
+                        <small>KEEP IN TOUCH</small>
+                        <h2>Travel with us</h2>
+                    </div>
+
+                    <div className="inputDiv flex">
+                        <input type="text" placeholder="Enter Email Address" />
+                        <button className="btn flex" type="submit">
+                            SEND <FiSend className="icon" />
+                        </button>
+                    </div>
+                </div>
+
+                <div className="footerCard flex">
+                    <div className="footerIntro flex">
+                        <div className="logoDiv">
+                            <a href="#" className="logo flex">
+                                <h1>ESTOMA <MdOutlineTravelExplore className="icon" /> TRAVELS </h1>
+                            </a>
+                        </div>
+
+                        <div className="footerParagraph">
+                            Discover amazing destinations with Travel and Tours. Book your next adventure today!
+                        </div>
+
+                        <div className="footerSocials">
+                            <AiOutlineTwitter className="icon" />
+                            <AiFillYoutube className="icon" />
+                            <AiFillInstagram className="icon" />
+                            <FaTripadvisor className="icon" />
+                        </div>
+                    </div>
+
+                    <div className="footerLinks grid">
+                        {/* 1st Group */}
+                        <div className="linkGroup">
+                            <span className="groupTitle">
+                                OUR AGENCY
+                            </span>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Services
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Insurance
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Agency
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Tourism
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Payment
+                            </li>
+                        </div>
+
+                        {/* 2nd Group */}
+                        <div className="linkGroup">
+                            <span className="groupTitle">
+                                PARTNERS
+                            </span>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Bookings
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Rent cars
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Hostel World
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Trivago
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                TripAdvisor
+                            </li>
+                        </div>
+
+                        {/* 3rd Group */}
+                        <div className="linkGroup">
+                            <span className="groupTitle">
+                                LAST MINUTE
+                            </span>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                London
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                California
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Indonesia
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Europe
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Oceania
+                            </li>
+                        </div>
+                    </div>
+
+                    <div className="footerDiv flex">
+                        <small>© 2024 Estoma Travels.</small>
+                        <small> All rights reserved.</small>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
